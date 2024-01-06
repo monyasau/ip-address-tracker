41d5a14ef9f005428fc3d2a1920a3f93cb0bc9f7
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-01-06 22:49:00 +0100

    update: linked new component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TopNav from "./components/TopNav";
+import IpMain from "./components/IpMain";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <TopNav />
+      <IpMain/>
     </>
   );
 }
